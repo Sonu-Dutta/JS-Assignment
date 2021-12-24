9ac9f4503429ceeaacc378bd9bb360695cdd6b2e
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-24 11:24:00 +0530

    Update Q5_Even_or_Odd.js

--- Q5_Even_or_Odd.js
+++ Q5_Even_or_Odd.js
@@ -1,6 +1,9 @@
 // Check whether the number is even or odd
-let a=34;
-if(a%2==0){
+let a=0;
+if(a==0){
+    console.log("The number is neither even nor odd.");
+}
+else if(a%2==0){
     console.log("The number is even.");
 }
 else{
